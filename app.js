18ec01ba16ca718ef7643cba2911fb1b6677fa6a
Nick <nick.joat@gmail.com>
2024-06-25 16:45:17 -0700

    made corrections to onclick and keyup 'enter' functions, also made small styling corrections to search bar. Finally was able to correctly slice down the results to show only 6 movies.

--- app.js
+++ app.js
@@ -28,26 +28,31 @@ async function searchMovies(searchTerm) {
     const res = await fetch(`${URL}`);
     const data = await res.json();
     const movieData = await data.Search
-    const movieIdArr = movieData.map(elem => elem.imdbID)
-    const slicedIdArr = ((movieIdArr).slice(0, 6))
-    // this is where i started to encounter confusion while trying to slice the array down to only 6 //
-    console.log(movieData)
-    console.log(slicedIdArr)
+    const slicedMovieData = await movieData.slice(0, 6) 
     
-    moviesWrapperEl.innerHTML = movieData.map((movie) => renderMovies(movie)).join("")
+    moviesWrapperEl.innerHTML = slicedMovieData.map((movie) => renderMovies(movie)).join("")
 }
 
 function findMovies() {
     movieSearch.addEventListener("keypress", function(event) {
         let searchTerm = (movieSearch.value)
         if (event.key === "Enter") {
-          event.preventDefault();
-          searchMovies(searchTerm);
+            event.preventDefault();
+            console.log('enter was pressed')
+          return searchMovies(searchTerm);
         }
     });
 }
 
+function clickSearchBtn() {
+    let searchTerm = (movieSearch.value)
+    console.log('search was clicked')
+    return searchMovies(searchTerm)
+}
+
 function renderMovies(movie) {
+    if (movie.Poster == "N/A")
+        movie.Poster = "assets/404-error-not-found-page-lost-2327795402.png"
     return `<div class="movie">
         <div class="movie__tile">
             <div class="movie__img--box">
@@ -65,50 +70,3 @@ function renderMovies(movie) {
         </div>
     </div>`
 }
-
-/** extremely frustrated by this project
- * I can't figure out how to show the movie title search results next to 
- * the "search results" area like David's car app example, I can't even google it well enough to get
- * a stackoverflow thread on it.. 
- * My hope was to fetch the searched movies by title in searchbar (successfully did that)
- * then i wanted to plug the imdbID #s from that initial response, back into a 2nd
- * dynamic URL (similar to the first process) so that the returned data would have
- * detailed movie information that I could then plug into my dynamic html thereby creating
- * multiple movie tiles each with detailed information, instead of just the title and year released
- * that the initial API fetch provides.
- * 
- * feels like I wasted 4 days on something that either isnt possible or is just too advanced
- * 
-//  * async function getMovieDetails(movieId) {
-//     let detailURL = `https://www.omdbapi.com/?apikey=c24e8ce7&i=${movieId}&plot=full`
-//     let detailRes = await fetch(`${detailURL}`);
-//     const detailData = await detailRes.json();
-//     console.log(detailData)
-// }
-
-// getMovieDetails(movieId)
-
-
- *  Not happy with my result.
- * 
- * Did a huddle call via slack with Jose Hernandez on Saturday 6-22, he basically told me that what I was
- * trying to do was not ppssible, couldn't explain why or clarify just said not to worry about it. 
- * 
- * at time of submit, I can not get the following to work: 
- * #1 - the onclick() of my search button to activate the search input field. 
- * #2 - to trigger the loading "spinner" properly in my JS.
- * #3 - format the movie tiles that load to be uniform in height, the movie title
- * text is wraps like I want it to, but not sure how to solve the unform size of each tile.
- * #4 - how to .slice down my array to only show 6 results. (i spent a lot of time Saturday on this)
- * 
- * i feel like Thursday and Friday I flew through this project,
- * even set up a modal on the sign in button for practice just because
- * Then spent all day Saturday working on the "final touches" trying to fetch 
- * detailed movie info which tied my brain in knots and confused me so much
- * that I undermined my progress chasing my tail in circles
- * by monday I barely understand what I did even with the JavaScript that works.
- * 
- * I confused myself so badly, I feel totally lost. not satisfied with my final proj result
- * extremely frustrated.
- */
-
